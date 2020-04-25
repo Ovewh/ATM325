d0fb6db014e070b9d5230aa686ba31e26f1db2a6
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-04-25 14:05:55 +0200

    Update

--- Exercise6/plotMothlyGridded.py
+++ Exercise6/plotMothlyGridded.py
@@ -7,17 +7,20 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 import os
 
-def plotMonthlyGridded(path, title_plot ,file_title, vmax):
+def plotMonthlyGridded(path, title_plot ,file_title=None, vmax=None):
     dset = xr.open_dataset(path)
     lons = dset.lon
     lats = dset.lat
     data = dset[list(dset.keys())[0]].transpose()
-
+    plt.cla()
     fig = plt.figure(figsize=(10,8))
     
     ax = plt.axes(projection = ccrs.PlateCarree())
     ax.set_title(title_plot, fontsize = 16)
-    im = ax.pcolormesh(lons, lats, data, transform = ccrs.PlateCarree(), vmin = 0, vmax = vmax)
+    if vmax == None:
+        im = ax.pcolormesh(lons, lats, data, transform = ccrs.PlateCarree(), vmin = 0) 
+    else:
+        im = ax.pcolormesh(lons, lats, data, transform = ccrs.PlateCarree(), vmin = 0, vmax = vmax)
     gl = ax.gridlines(crs = ccrs.PlateCarree(), draw_labels = True, 
                         color = 'grey', alpha =0.6, linestyle = '--')
     gl.xlabels_top = False
@@ -25,8 +28,13 @@ def plotMonthlyGridded(path, title_plot ,file_title, vmax):
     ax.coastlines()
     cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])
     plt.colorbar(im,cax=cax,label = data.units, extend='max')
-    plt.savefig(('figs/{}.png').format(file_title), dpi=300,bbox_inches='tight')
-    plt.cla()
+    if file_title == None:
+        return fig, ax
+    
+    else:
+        plt.savefig(('figs/{}.png').format(file_title), dpi=300,bbox_inches='tight')
+        return fig, ax
+        
 
 if __name__ == "__main__":
     direcOmi = 'data/OMI_NO2/'
--- Exercise6/plotYearlyGridded.py
+++ Exercise6/plotYearlyGridded.py
@@ -8,7 +8,7 @@ import numpy as np
 import os
 
 
-def plot_yearly_gridded(path, vmax, cmap ='YlOrBr'):
+def plot_yearly_gridded(path, vmax, title=None, save=False ,cmap ='YlOrBr'):
     dset = xr.open_dataset(path)
     lons = dset.lon
     lats = dset.lat
@@ -17,8 +17,11 @@ def plot_yearly_gridded(path, vmax, cmap ='YlOrBr'):
 
     fig =  plt.figure(figsize=(12,8))
     ax = plt.axes(projection = ccrs.PlateCarree())
-    ax.set_title(dset.attrs['plot_hint_title'] + '\n' + 
+    if title == None:
+        ax.set_title(dset.attrs['plot_hint_title'] + '\n' + 
                     dset.attrs['plot_hint_subtitle'] , fontsize = 14)
+    else:
+        ax.set_title(title, fontsize = 14)
     gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, color = 'grey', 
                     alpha=0.6, linestyle = '--')
     gl.xlabels_top = False
@@ -31,11 +34,17 @@ def plot_yearly_gridded(path, vmax, cmap ='YlOrBr'):
     cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])
     plt.colorbar(im,cax=cax,label = data.units, extend='max')
     ax.coastlines()
-    plt.savefig(('figs/{}_{}.png').format(data.product_short_name, dset.start_time[:4]), dpi=300,bbox_inches='tight')
+    if save:
+        plt.savefig(('figs/{}_{}.png').format(data.product_short_name, dset.start_time[:4]), dpi=300,bbox_inches='tight')
+        return fig, ax
+    else:
+        return fig, ax
 
 if __name__ == "__main__":
     for path in os.listdir('data/NO2/'):
-        plot_yearly_gridded('data/NO2/'+ path ,2e16, cmap='viridis')
+        plot_yearly_gridded('data/NO2/'+ path ,2e16, cmap='viridis', save=True)
+        plt.cla()
     for path in os.listdir('data/SO2/'):
-        plot_yearly_gridded('data/SO2/'+ path ,2.7, cmap='viridis')
+        plot_yearly_gridded('data/SO2/'+ path ,2.7, cmap='viridis', save=True)
+        plt.cla()
 
